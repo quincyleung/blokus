2a3939dfa1ed655835d294d91d5195fa58b1ea0c
Quincy Leung <quincyleung@uchicago.edu>
2024-05-02 07:23:08 -0500

    updated bot

--- src/bot.py
+++ src/bot.py
@@ -3,28 +3,28 @@ from fakes import BlokusStub
 from piece import Point, Shape, Piece
 
 # Create a BlokusStub object with two players and board size 14x14
-test = BlokusStub(2, 14, ((4,4), (9,9)))
+test = BlokusStub(2, 14, {(4,4), (9,9)})
 NUM_GAMES = int(sys.argv[1])
 
 # Keep track of the fact that there are two bots playing. 
-# Both follow the same strategy: picking a location at random from the board. 
-# Make sure to use the available_moves method
+# Both follow the same strategy: picking a location at random from the board.
 bot0_wins = 0
 bot1_wins = 0
 tie = 0
 
 for i in range(NUM_GAMES):
-    # Each bot will make moves until game is over (according to game_over method). 
-    # A bot retires only if available_moves says there are no available moves left
+    # Each bot will make moves until game is over. A bot retires only if available_moves says there are no available moves left
     while not test.game_over:        
         if len(test.available_moves()) > 0:
+            print("available moves:", test.available_moves(), "len", len(test.available_moves()))
             next_piece = random.choice(list(test.available_moves()))
+            print("next piece:", next_piece)
             if test.legal_to_place(next_piece):
                 test.maybe_place(next_piece)
+                print("placed ", next_piece)
         else:
             test.retire
 
-    # If there is a winner, add one to that bot's tally
     print("winner:", test.winners)
     if len(test.winners) == 1:
         if 1 in test.winners:
@@ -34,7 +34,6 @@ for i in range(NUM_GAMES):
     elif len(test.winners) == 2:
         tie += 1
 
-print("Num games:", NUM_GAMES)
 print("Bot 0 Wins |  ", bot0_wins, bot0_wins/NUM_GAMES * 100, "%")
 print("Bot 1 Wins |  ", bot1_wins, bot1_wins/NUM_GAMES * 100, "%")
 print("Ties       | ", tie, tie/NUM_GAMES * 100, "%")
