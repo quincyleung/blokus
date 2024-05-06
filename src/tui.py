714d4ad3349532051a84d82e79ad1acb758b03a3
Minseo Kim <minseokim1015@gmail.com>
2024-05-06 16:22:38 -0500

    tui milestone 1 complete

--- src/tui.py
+++ src/tui.py
@@ -1,61 +1,64 @@
 """
-referred to connectm's tui.py implementation for inspiration
+referred to connectm's tui.py implementation for inspiration 
+on constructing the grid and utilizing colorama
 """
 import colorama
-from colorama import Fore, Style
+from colorama import Fore
+import sys
 
 from blokus import BlokusBase, Blokus, ShapeKind
 from fakes import BlokusStub, BlokusFake
 
-grid: BlokusStub
-# grid: list[list[Optional[tuple[int, ShapeKind]]]]
+# milestone 1 to 2 change: board input is BlokusFake instead of BlokusStub
+board: BlokusFake
+SIZE = int(sys.argv[1])
 
 # printing board (visual display with gui)
-def print_board(grid: BlokusStub):
+def print_board(board: BlokusFake):
     colorama.init() # to color symbols
 
     # grid's top edge
-    print(Fore.BLACK + "┎" + ("┮" * (grid.size - 1)) + "┑")
+    print(Fore.BLACK + "┌──" + ("┬──" * (board.size - 1)) + "┐")
+
     # sides and inner sections (between spots)
-    for x in range(grid.size):
+    for x in range(board.size):
         base = Fore.BLACK + "│"
-        for y in range(grid.size):
-            player = grid[x][y][0]
-            piece = grid[x][y][1]
+        for y in range(board.size):
             # None: place empty space in it
-            if grid[x][y] is None:
-                base += " "
+            if board.grid[x][y] is None:
+                base += "  "
             # process per player (1 and 2)
-            elif player == 1:
-                base += " "
-                for section in piece:
-                    base[x + section[0], y + section[1]] = Fore.BLUE + "█"
-            elif player == 2:
-                base += " "
-                for section in piece:
-                    base[x + section[0], y + section[1]] = Fore.RED + "█"
+            else:
+                player = board.grid[x][y][0]
+                piece = board.grid[x][y][1]
+                if player == 1:
+                    if piece is not None:
+                        base += Fore.BLUE + "██"
+                    else:
+                        base += "  "
+                elif player == 2:
+                    if piece is not None:
+                        base += Fore.RED + "██"
+                    else:
+                        base += "  "
             base += Fore.BLACK + "│"
         print(base)
 
         # middle sections or bottom edge
-        if x < grid.size - 1: # not at end yet
-            print(Fore.BLACK + "┡" + ("╁" * (grid.size - 1)) + "┥")
+        if x < board.size - 1: # not at end yet
+            print(Fore.BLACK + "├──" + ("┼──" * (board.size - 1)) + "┤")
         else: # at end (x == row)
-            print(Fore.BLACK + "┕" + ("┵" * (grid.size - 1)) + "┙") # symbols are too thin?
+            print(Fore.BLACK + "└──" + ("┴──" * (board.size - 1)) + "┘") # symbols are too short?
 
-# Future tasks
 def play_blokus(blokus: BlokusBase) -> None:
     # game loop: ends when player/players win (all pieces are placed)
     while True:
         print(blokus)
-        # TODO: game process
         # If there is a winner, break out of the loop
         if BlokusFake.winners is not None:
             break
-        # TODO: Update the player - switch between the different players
 
 if __name__ == "__main__":
-    #play_blokus()
-    #game = 
-    #print_board(game)
-    raise NotImplementedError
+    board_size = SIZE
+    board: BlokusFake = BlokusFake(2, board_size, {(0, 0), (board_size - 1, board_size - 1)})
+    print_board(board)
