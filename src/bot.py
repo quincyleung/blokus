71154f75bce1db8afa276408a3b42f2619399530
Quincy Leung <quincyleung@uchicago.edu>
2024-05-07 22:00:29 -0500

    Created classes for two bots and restructured test code

--- src/bot.py
+++ src/bot.py
@@ -1,33 +1,106 @@
+"""
+Bots for Blokus
+"""
 import sys
 import random
-from fakes import BlokusStub
+
+from abc import ABC, abstractmethod
+from typing import Optional
+
+from base import BlokusBase, Grid
+from piece import Point, Shape, Piece
+from shape_definitions import ShapeKind, definitions
+from blokus import Blokus
+
+#
+# BOTS
+#
+
+class NBot:
+    """
+    Needs Improvement Bot that just picks a move at random.
+    """
+
+    def __init__(self, blokus: Blokus, player_num: int):
+        """
+        Constructor.
+
+        Args:
+            blokus: The Blokus board
+            player_num: Bot's player number
+        """
+        self._blokus = blokus
+        self._player_num = player_num
+    
+    def suggest_move(self) -> Piece:
+        """
+        Suggests a move at random
+
+        Returns: None
+        """
+        return random.choice(list(board.available_moves()))
+
+class SBot:
+    """
+    Satisfactory Bot that incorporates simple Blokus-specific heuristics. 
+    Will do the following:
+
+    - If there is a larger piece, it will select it.
+    - Otherwise, pick a piece at random.
+    """
+
+    def __init__(self, blokus: Blokus, player_num: int):
+        """
+        Constructor.
+
+        Args:
+            blokus: The Blokus board
+            player_num: Bot's player number
+        """
+        self._blokus = blokus
+        self._player_num = player_num
+    
+    def suggest_move(self) -> Piece:
+        """
+        Suggests a move
+
+        Returns: None
+        """
+        raise NotImplementedError
+
+#
+# SIMULATION CODE
+#
 
 NUM_GAMES: int = int(sys.argv[1])
-bot0_wins: int = 0
 bot1_wins: int = 0
+bot2_wins: int = 0
 tie: int = 0
 
 for i in range(NUM_GAMES):
-    # Create a BlokusStub object with two players and board size 14x14
-    test = BlokusStub(2, 14, {(4,4), (9,9)})
+    # Create a Blokus object with two players, board size 11x11 and two start
+    # positions in the upper-left (0,0) and bottom-right (10,10) corners
+    board = Blokus(num_players=2, size=11, start_positions={(0,0), (10,10)})
 
-    while not test.game_over:
-        if len(test.available_moves()) > 0:
-            # Picking a location at random
-            next_piece = random.choice(list(test.available_moves()))
-            if test.legal_to_place(next_piece):
-                test.maybe_place(next_piece)
+    bot1 = NBot(board, 1)
+    bot2 = SBot(board, 2)
+
+    while not board.game_over:
+        if board.curr_player == 1 and len(board.available_moves()) > 0:
+            board.maybe_place(bot1.suggest_move())
+        elif board.curr_player == 2 and len(board.available_moves()) > 0:
+            board.maybe_place(bot2.suggest_move())
         else:
-            test.retire()
+            board.retire()
 
-    if len(test.winners) == 1:
-        if 1 in test.winners:
-            bot0_wins += 1
-        elif 2 in test.winners:
+    if len(board.winners) == 1:
+        if 1 in board.winners:
             bot1_wins += 1
-    elif len(test.winners) == 2:
+        elif 2 in board.winners:
+            bot2_wins += 1
+    elif len(board.winners) == 2:
         tie += 1
 
-print("Bot 0 Wins |  ", bot0_wins/NUM_GAMES * 100, "%")
 print("Bot 1 Wins |  ", bot1_wins/NUM_GAMES * 100, "%")
+print("Bot 2 Wins |  ", bot2_wins/NUM_GAMES * 100, "%")
 print("Ties       | ", tie/NUM_GAMES * 100, "%")
