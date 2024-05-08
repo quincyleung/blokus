18e1db2fa70951e3fa29cb57aadf7fd7fb5a95cf
Quincy Leung <quincyleung@uchicago.edu>
2024-05-07 22:21:37 -0500

    Commented out print statements and added upper bound for board in legal_to_place()

--- src/blokus.py
+++ src/blokus.py
@@ -281,23 +281,23 @@ class Blokus(BlokusBase):
         else:
             for point in piece.squares():
                 r, c = point
-                print("point row:", r, "point col:", c)
+                #print("point row:", r, "point col:", c)
                 for row_index in range(r - 1, r + 2):
                     for col_index in range(c - 1, r + 2):
-                        if row_index >= 0 and col_index >= 0:
+                        if (0 <= row_index < self.size) and (0 <= col_index < self.size):
                             grid_value = self.grid[row_index][col_index]
-                            print("checking row index: ", row_index, "checking col index:", col_index)
-                            print("grid val:", grid_value)
+                            #print("checking row index: ", row_index, "checking col index:", col_index)
+                            #print("grid val:", grid_value)
                             index = (row_index, col_index)
                             
                             if grid_value is not None:
                                 if index == (r-1, c-1) or index == (r-1, c+1) or index == (r+1, c-1) or index == (r+1, c+1):
-                                    print("has corner case!", index)
-                                    print("grid val", grid_value[0], "player:", self.curr_player)
+                                    #print("has corner case!", index)
+                                    #print("grid val", grid_value[0], "player:", self.curr_player)
                                     if grid_value[0] == self.curr_player:
                                         return True
                                 elif grid_value[0] == self.curr_player:
-                                    print("index:", index, "return false!")
+                                    #print("index:", index, "return false!")
                                     return False
         return False
 
@@ -388,7 +388,7 @@ class Blokus(BlokusBase):
                     piece.set_anchor((r, c)) # Testing different locations
                     if self.legal_to_place(piece):
                         available_moves.add(piece)
-                        
+
                     # Check different orientations
                     # for _ in range(4):
                     #     piece.rotate_left() 
