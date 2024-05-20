3bdb044a3eb66df87ae853ee1d6ae9a9824aaed1
Grace Lu <Grace@MacBook-Air-5.local>
2024-05-20 18:30:03 -0500

    finished milestone 3

--- src/grid.py
+++ src/grid.py
@@ -42,25 +42,20 @@ def string_to_grid(s: str) -> Grid:
     """
     lines = s.strip().split('\n') # split by newlines to get rows
     lines = lines[2:-1] # remove column indices, top and bottom borders
+    #print(f"lines: {lines}")
     grid_result = []
 
     for line in lines:
-        line = line.strip()[2:-2] #remove R and L borders, trim spaces
+        line = line[line.find('||')+2:line.rfind('||')] #remove row indices, R and L borders
         grid_row = []
-        
         for i in range(0, len(line), 2): #process each cell in pairs
-            if i + 1 < len(line):
-                if line[i] == ' ' and line[i + 1] == ' ':
-                    grid_row.append(None) #two spaces mean empty cell
-                elif line[i] == ' ': #first space empty means P2
-                    grid_row.append((2, ShapeKind(line[i + 1])))
-                elif line[i + 1] == ' ': #second space empty means P1
-                    grid_row.append((1, ShapeKind(line[i])))
-            else:
-                if line[i] == ' ':
-                   grid_row.append(None)
-                else:
-                    grid_row.append((1, ShapeKind(line[i]))) 
+            cell = line[i:i+2]
+            if cell == '  ':
+                grid_row.append(None) #two spaces mean empty cell
+            elif cell[0] == ' ': #first space empty means P2
+                grid_row.append((2, ShapeKind(cell[1])))
+            elif cell[1] == ' ': #second space empty means P1
+                grid_row.append((1, ShapeKind(cell[0])))
         grid_result.append(grid_row)
     return grid_result
 
@@ -117,10 +112,8 @@ def test_grid_2() -> None:
         ||       2 2|| 4
         ||||||||||||||
         """
-    print(grid_to_string(grid))
-    print('  ' + dedent(s).strip())
-    assert '  ' + dedent(s).strip() == grid_to_string(grid)
 
+    assert '  ' + dedent(s).strip() == grid_to_string(grid)
     assert grid == string_to_grid(grid_to_string(grid))
 
 # Test Blokus Duo, P1 places ONE at (4, 4) and P2 places ONE at (9, 9)
@@ -157,70 +150,73 @@ def test_grid_3() -> None:
         ||||||||||||||||||||||||||||||||
         """
 
-    generated_str = grid_to_string(grid).strip()
-    expected_str = dedent(s).strip()
-    generated_str = dedent(generated_str).strip()
-    assert expected_str == generated_str
-
-    assert grid == string_to_grid(grid_to_string(grid))
+    assert '  ' + dedent(s).strip() == grid_to_string(grid)
+    assert grid == string_to_grid(grid_to_string(grid).strip())
 
 # Test Blokus Duo, P1 places C at (4, 4) and P2 places TWO at (9, 9)
 def test_grid_4() -> None:
-    blokus = test_blokus.test_init_blokus_mini_2()
+    blokus = test_blokus.test_init_blokus_duo_2()
 
-    # P1 plays C at (0, 0)
+    # P1 plays C at (4, 4)
     piece_c = Piece(blokus.shapes[ShapeKind.C])
-    piece_c.set_anchor((0, 0))
+    piece_c.set_anchor((4, 4))
     blokus.maybe_place(piece_c)
 
-    # P2 plays TWO at (4, 3)
+    # P2 plays TWO at (9, 9)
     piece_two = Piece(blokus.shapes[ShapeKind.TWO])
-    piece_two.set_anchor((4, 3))
+    piece_two.set_anchor((9, 9))
     blokus.maybe_place(piece_two)
 
     grid = blokus.grid
     s = """
-          0 1 2 3 4   
-        ||||||||||||||
-        ||C C       || 0
-        ||C         || 1
-        ||          || 2
-        ||          || 3
-        ||       2 2|| 4
-        ||||||||||||||
+          0 1 2 3 4 5 6 7 8 9 0 1 2 3   
+        ||||||||||||||||||||||||||||||||
+        ||                            || 0
+        ||                            || 1
+        ||                            || 2
+        ||                            || 3
+        ||        C C                 || 4
+        ||        C                   || 5
+        ||                            || 6
+        ||                            || 7
+        ||                            || 8
+        ||                   2 2      || 9
+        ||                            || 10
+        ||                            || 11
+        ||                            || 12
+        ||                            || 13
+        ||||||||||||||||||||||||||||||||
         """
-    print(grid_to_string(grid))
-    print('  ' + dedent(s).strip())
+
     assert '  ' + dedent(s).strip() == grid_to_string(grid)
+    assert grid == string_to_grid(grid_to_string(grid).strip())
 
-    assert grid == string_to_grid(grid_to_string(grid))
-
-# Test Blokus Classic, P1 places ONE at (0, 0) and P2 places ONE at (19, 19)
+# Test Blokus Mono, P1 places ONE at (5, 5)
 def test_grid_5() -> None:
-    blokus = test_blokus.test_init_blokus_mini_2()
+    blokus = test_blokus.test_init_blokus_mono()
 
-    # P1 plays ONE at (0, 0)
+    # P1 plays ONE at (5, 5)
     piece_one = Piece(blokus.shapes[ShapeKind.ONE])
-    piece_one.set_anchor((0, 0))
-    blokus.maybe_place(piece_one)
-
-    # P2 plays TWO at (4, 4)
-    piece_one.set_anchor((4, 4))
+    piece_one.set_anchor((5, 5))
     blokus.maybe_place(piece_one)
 
     grid = blokus.grid
     s = """
-          0 1 2 3 4   
-        ||||||||||||||
-        ||C C       || 0
-        ||C         || 1
-        ||          || 2
-        ||          || 3
-        ||       2 2|| 4
-        ||||||||||||||
+          0 1 2 3 4 5 6 7 8 9 0   
+        ||||||||||||||||||||||||||
+        ||                      || 0
+        ||                      || 1
+        ||                      || 2
+        ||                      || 3
+        ||                      || 4
+        ||          1           || 5
+        ||                      || 6
+        ||                      || 7
+        ||                      || 8
+        ||                      || 9
+        ||                      || 10
+        ||||||||||||||||||||||||||
         """
-    print(grid_to_string(grid))
-    print('  ' + dedent(s).strip())
-    assert '  ' + dedent(s).strip() == grid_to_string(grid)
 
-    assert grid == string_to_grid(grid_to_string(grid))
+    assert '  ' + dedent(s).strip() == grid_to_string(grid)
+    assert grid == string_to_grid(grid_to_string(grid).strip())
